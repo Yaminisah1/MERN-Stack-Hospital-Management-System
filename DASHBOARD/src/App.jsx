a9ca6f391f0bd69074a661e8a56bea3c52530a92
Yamini Sah <141642913+Yaminisah1@users.noreply.github.com>
2024-07-21 16:36:18 +0530

    Update App.jsx

--- DASHBOARD/src/App.jsx
+++ DASHBOARD/src/App.jsx
@@ -23,7 +23,7 @@ const App = () => {
     const fetchUser = async () => {
       try {
         const response = await axios.get(
-          "http://localhost:4000/api/v1/user/admin/me",
+          "https://hospital-management-backend-q83p.onrender.com/api/v1/user/login/api/v1/user/admin/me",
           {
             withCredentials: true,
           }
