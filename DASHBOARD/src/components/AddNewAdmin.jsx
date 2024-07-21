306f7060c5c9e447d6bd33e5266f55617c6508c3
Yamini Sah <141642913+Yaminisah1@users.noreply.github.com>
2024-07-21 17:25:02 +0530

    Update AddNewAdmin.jsx

--- DASHBOARD/src/components/AddNewAdmin.jsx
+++ DASHBOARD/src/components/AddNewAdmin.jsx
@@ -23,7 +23,7 @@ const AddNewAdmin = () => {
     try {
       await axios
         .post(
-          "https://hospital-management-backend-q83p.onrender.com/api/v1/user/login/api/v1/user/admin/addnew",
+          "https://hospital-management-backend-q83p.onrender.com/api/v1/user/admin/addnew",
           { firstName, lastName, email, phone, nic, dob, gender, password },
           {
             withCredentials: true,
