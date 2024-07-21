f1a6e93bed9a5c7647582be33de0724c83b154c5
Yamini Sah <141642913+Yaminisah1@users.noreply.github.com>
2024-07-21 17:27:39 +0530

    Update Doctors.jsx

--- DASHBOARD/src/components/Doctors.jsx
+++ DASHBOARD/src/components/Doctors.jsx
@@ -11,7 +11,7 @@ const Doctors = () => {
     const fetchDoctors = async () => {
       try {
         const { data } = await axios.get(
-          "https://hospital-management-backend-q83p.onrender.com/api/v1/user/login/api/v1/user/doctors",
+          "https://hospital-management-backend-q83p.onrender.com/api/v1/user/doctors",
           { withCredentials: true }
         );
         setDoctors(data.doctors);
