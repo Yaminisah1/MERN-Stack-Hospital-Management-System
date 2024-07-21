04d6de9042f0ae1898adbb4391ea50dc1f5a83d2
Yamini Sah <141642913+Yaminisah1@users.noreply.github.com>
2024-07-21 16:39:02 +0530

    Update Login.jsx

--- DASHBOARD/src/components/Login.jsx
+++ DASHBOARD/src/components/Login.jsx
@@ -18,7 +18,7 @@ const Login = () => {
     try {
       await axios
         .post(
-          "http://localhost:4000/api/v1/user/login",
+          "https://hospital-management-backend-q83p.onrender.com/api/v1/user/login/api/v1/user/login",
           { email, password, confirmPassword, role: "Admin" },
           {
             withCredentials: true,
