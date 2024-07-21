d2386888d50b580beebdf776b61f82fd5dfc01f0
Yamini Sah <141642913+Yaminisah1@users.noreply.github.com>
2024-07-21 17:27:06 +0530

    Update AddNewDoctor.jsx

--- DASHBOARD/src/components/AddNewDoctor.jsx
+++ DASHBOARD/src/components/AddNewDoctor.jsx
@@ -58,7 +58,7 @@ const AddNewDoctor = () => {
       formData.append("doctorDepartment", doctorDepartment);
       formData.append("docAvatar", docAvatar);
       await axios
-        .post("https://hospital-management-backend-q83p.onrender.com/api/v1/user/login/api/v1/user/doctor/addnew", formData, {
+        .post("https://hospital-management-backend-q83p.onrender.com/api/v1/user/doctor/addnew", formData, {
           withCredentials: true,
           headers: { "Content-Type": "multipart/form-data" },
         })
